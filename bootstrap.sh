f827366751a62b6ae81498ff94e8223450d1464a
apj68 <freelist@mailc.net>
2026-07-15 20:58:28 -0700

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REPO="https://github.com/apj/cm.git"
+BACKUP_HOST="tailinsula"
+BACKUP_SHARE="/volume1/NetBackup"
+MOUNT="/mnt/netbackup"
+
+sudo apt update
+
+sudo apt install -y \
+    git \
+    curl \
+    nfs-common
+
+git clone "$REPO"
+
+cd cm.git
+
+./bootstrap-local.sh
