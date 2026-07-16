99a647434112c96205ac329fbd8462263b1a26d2
apj68 <freelist@mailc.net>
2026-07-15 21:10:44 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO="https://github.com/apj/cm.git"
+REPO="git@github.com/apj/cm.git"
 BACKUP_HOST="tailinsula"
 BACKUP_SHARE="/volume1/NetBackup"
 MOUNT="/mnt/netbackup"
+
+restore_file() {
+    cp -pr $1 $HOME
+}
 
 sudo apt update
 
 sudo apt install -y \
     git \
     nfs-common
+
+sudo mount -t nfs ${BACKUP_HOST}:${BACKUP_SHARE} $MOUNT
+
+restore_file ".ssh"
+restore_file ".gitconfig"
+restore_file ".profile"
+restore_file ".aliases"
 
 git clone "$REPO"
 
